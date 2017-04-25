b8335b64bb3e8b0d35835d916bde26363a8dec2f
anandganesh7 <dnana.7ea@gmail.com>
2017-04-24 18:54:21 -0700

    xss: form element search

--- xss/find_elements.py
+++ xss/find_elements.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
 request = requests.get("http://check.cyberpersons.com/crossSiteCheck.html")
-
 parseHTML = BeautifulSoup(request.text, 'html.parser')
-
 htmlForm = parseHTML.form
-
 formName = htmlForm['name']
-
 print "Form name: " + formName
+
+# this searches for a form element in the html page and parses it
